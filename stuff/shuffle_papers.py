8ba2bfc6027119b6d20e1a2cdd6c398ef886d3e3
Jan Grewe <jan.grewe@g-node.org>
2016-11-13 13:05:00 +0100

    [shuffle_papers] fix py2/3 version issues

--- stuff/shuffle_papers.py
+++ stuff/shuffle_papers.py
@@ -1,38 +1,40 @@
-__author__ = 'jan'
-
 import numpy as np
-import matplotlib.pyplot as plt
+import sys
 from random import shuffle
 from random import seed
-from IPython import embed
+if sys.version_info > (2,):
+    Raw_input = input
 
 if __name__ == '__main__':
-
     paper_nr = np.arange(15)
     seed(float(np.random.rand(1)))
     shuffle(paper_nr)
 
-    paper_dict = {1: ['14. 12', 'Hagedorn & Heiligenberg, 1985'],
-                  2: ['14. 12', 'v. d. Emde & Blackman, 1997'],
-                  3: ['15. 12', 'Stamper, 2010'],
-                  4: ['15. 12', 'Post & v. d. Emde, 1999'],
-                  5: ['16. 12', 'Stoddard, 2008'],
-                  6: ['16. 12', 'Carlson, 2011'],
-                  7: ['17. 12', 'Catania, 2014'],
-                  8: ['17. 12', 'Catania, 2015'],
-                  9: ['11. 01', 'Zupanc et al., 2003'],
-                  10: ['11. 01', 'Hupe et al., 2008'],
-                  11: ['12. 01', 'Kawasaki, 1997'],
-                  12: ['12. 01', 'Rose, 2004'],
-                  13: ['13. 01', 'McAnnelly, 2000'],
-                  14: ['13.01', 'Nelson, 1999'],
-                  15: ['14. 01', 'Kreysing, 2012']}
+    paper_dict = {1: ['21.11', 'Hagedorn & Heiligenberg, 1985'],
+                  2: ['21.11', 'v. d. Emde & Blackman, 1997'],
+                  3: ['22.11', 'Stamper, 2010'],
+                  4: ['22.11', 'Post & v. d. Emde, 1999'],
+                  5: ['23.11', 'Stoddard, 2008'],
+                  6: ['23.11', 'Carlson, 2011'],
+                  7: ['24.11', 'Catania, 2014'],
+                  8: ['24.11', 'Catania, 2015'],
+                  9: ['28.11', 'Zupanc et al., 2003'],
+                  10: ['28.11', 'Hupe et al., 2008'],
+                  11: ['29.11', 'Kawasaki, 1997'],
+                  12: ['29.11', 'Rose, 2004'],
+                  13: ['30.11', 'McAnnelly, 2000'],
+                  14: ['30.11', 'Nelson, 1999'],
+                  15: ['01.12', 'Kreysing, 2012']}
 
     for enu, curr_nr in enumerate(paper_nr):
-
         paper_date = paper_dict[curr_nr][0]
         paper_name = paper_dict[curr_nr][1]
-        name = raw_input("\nHello there! What's your name?")
+        if sys.version_info < (3,):
+            name = raw_input("\nHello there! What's your name?")
+        else:
+            name = input("Hello there! What is your name?")
+        if name == "q" or name == "quit":
+            break
         print('\nNice to meet you, ' + name + ', you have to present paper # ' + str(curr_nr))
         print("The paper's name is " + paper_name + " and you will present it on " + paper_date)
 
