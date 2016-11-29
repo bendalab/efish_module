7d90343d6787792aa1062d3a3345d553c602a938
Jan Grewe <jan.grewe@g-node.org>
2016-11-29 13:55:24 +0100

    add method to read jar data

--- analysis/nix_helpers.py
+++ analysis/nix_helpers.py
@@ -85,6 +85,58 @@ def read_block_metadata(data_file):
     return metadata
 
 
+def read_jar_data(data_file, pre_stimulus_duration=5., post_stimulus_duration=20):
+    """
+    Liest die Daten von einem JAR Experiment.
+
+    :param data_file:
+    :param pre_stimulus_duration: Zeit bevor der Stimulus an war.
+    :param post_stimulus_duration:  Zeit nachdem der Stimulus aus war.
+    :return: pre_stimulus_duration: die wirklich vor dem Stimulus ausgelesene Zeit.
+             post_stimulus_duration: die tatsaechlich nach dem Stimulus ausgelesene Zeit.
+             duration: die Dauer der Stimulation
+             eod: die EOD in dem angegebenen Zeitraum
+             eod_times: die  ZEitpunkte der detektieren EODs
+             time: ein Zeitvektor, gleiche Laenge, wie eod
+             metadata: dictionary mit allen Metadaten
+
+    """
+    if not os.path.exists(data_file):
+        return None
+    f = nix.File.open(data_file, nix.FileMode.ReadOnly, backend="hdf5")
+    b = f.blocks[0]
+    m = b.metadata
+    metadata = nix_metadata_to_dict(m)
+    tag = None
+
+    for t in b.multi_tags:
+        if "ManualJAR" in t.name:
+            tag = t
+            break
+    start_time = tag.positions[:][0]
+    duration = tag.extents[:][0]
+    events_array = b.data_arrays['EOD_events']
+    eod_array = b.data_arrays["EOD"]
+    sampling_rate = 1./eod_array.dimensions[0].sampling_interval
+    start_time = start_time - pre_stimulus_duration
+    pre_stimulus_duration = pre_stimulus_duration if start_time > 0 else tag.positions[:][0]
+    end_time = start_time + duration + post_stimulus_duration
+    start_index = start_time * sampling_rate
+    end_index = end_time * sampling_rate
+    post_stimulus_duration = post_stimulus_duration if end_index < len(eod_array) else len(eod_array) / sampling_rate - start_time - duration
+    if start_index < 0:
+        start_index = 0
+        start_time = 0.
+    if end_index > len(eod_array):
+        end_index = len(eod_array) -1
+        end_time = end_index / sampling_rate
+    eod = eod_array[:][int(start_index) : int(end_index)]
+    times = events_array[:]
+    eod_times = times[(times > start_time) & (times < start_time + duration)]
+    f.close()
+    time = np.arange(len(eod)) / sampling_rate - pre_stimulus_duration
+    return pre_stimulus_duration, post_stimulus_duration, duration, eod, eod_times, time, metadata
+
 if __name__ == "__main__":
     # make sure to close the file before exiting! Take care, when using embed!
     datafile = '../configs/wave/2016-11-11-aa-wombat/2016-11-11-aa-wombat.nix.h5'
