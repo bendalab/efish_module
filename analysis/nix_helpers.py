87ff5e62476b06e73e93fcfce6e10bc07d0590f5
Jan Grewe <jan.grewe@g-node.org>
2017-11-27 23:54:49 +0100

    remove old version

--- analysis/nix_helpers.py
+++ analysis/nix_helpers.py
@@ -112,7 +112,7 @@ def read_df(data_file):
     return df, before, after, duration
 
 
-def read_jar_data2(data_file, count=-1):
+def read_jar_data(data_file, count=-1):
     if not os.path.exists(data_file):
         print("File  not found %s!" % data_file)
         return None, None
@@ -147,60 +147,6 @@ def read_jar_data2(data_file, count=-1):
     return data, dfs
 
 
-def read_jar_data(data_file, pre_stimulus_duration=5., post_stimulus_duration=20):
-    """
-    Liest die Daten von einem JAR Experiment.
-
-    :param data_file:
-    :param pre_stimulus_duration: Zeit bevor der Stimulus an war.
-    :param post_stimulus_duration:  Zeit nachdem der Stimulus aus war.
-    :return: pre_stimulus_duration: die wirklich vor dem Stimulus ausgelesene Zeit.
-             post_stimulus_duration: die tatsaechlich nach dem Stimulus ausgelesene Zeit.
-             duration: die Dauer der Stimulation
-             eod: die EOD in dem angegebenen Zeitraum
-             eod_times: die  ZEitpunkte der detektieren EODs
-             time: ein Zeitvektor, gleiche Laenge, wie eod
-             metadata: dictionary mit allen Metadaten
-
-    """
-    if not os.path.exists(data_file):
-        return None
-    f = nix.File.open(data_file, nix.FileMode.ReadOnly, backend="hdf5")
-    b = f.blocks[0]
-    m = b.metadata
-    metadata = nix_metadata_to_dict(m)
-    tag = None
-
-    for t in b.multi_tags:
-        if "ManualJAR" in t.name:
-            tag = t
-            break
-
-    stim_meta = nix_metadata_to_dict(tag.metadata)
-    metadata.update(stim_meta)
-
-    start_time = tag.positions[:][0]
-    duration = tag.extents[:][0]
-    events_array = b.data_arrays['EOD_events']
-    eod_array = b.data_arrays["EOD"]
-    sampling_rate = 1./eod_array.dimensions[0].sampling_interval
-
-    pre_stimulus_duration = pre_stimulus_duration if start_time > pre_stimulus_duration else tag.positions[:][0]
-    if (start_time + duration + post_stimulus_duration) * sampling_rate > len(eod_array):
-        post_stimulus_duration = len(eod_array) / sampling_rate - start_time - duration
-    end_time = start_time + duration + post_stimulus_duration
-
-    start_index = (start_time - pre_stimulus_duration) * sampling_rate
-    end_index = end_time * sampling_rate
-
-    eod = eod_array[:][int(start_index):int(end_index)]
-    times = events_array[:]
-    eod_times = times[(times > (start_time - pre_stimulus_duration)) & (times < end_time)] - start_time
-    f.close()
-    time = np.arange(len(eod)) / sampling_rate - pre_stimulus_duration
-    return pre_stimulus_duration, post_stimulus_duration, duration, eod, eod_times, time, metadata
-
-
 if __name__ == "__main__":
     # make sure to close the file before exiting! Take care, when using embed!
     datafile = '/Users/jan/zwischenlager/efish_kurs/data/chirps/2017-11-27-aa-amnesix/2017-11-27-aa-amnesix.nix'
@@ -208,5 +154,5 @@ if __name__ == "__main__":
     # events = read_eod_events(datafile)
     # info = read_subject_info(datafile)
     # b_info = read_block_metadata(datafile)
-    data, dfs = read_jar_data2(datafile)
+    data, dfs = read_jar_data(datafile)
     embed()
