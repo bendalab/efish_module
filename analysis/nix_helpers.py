252af4ceace93c8023efa51dbc3adba9bf91fd12
Jan Grewe <jan.grewe@g-node.org>
2017-11-27 23:54:06 +0100

    new version of reading jar files

--- analysis/nix_helpers.py
+++ analysis/nix_helpers.py
@@ -71,7 +71,7 @@ def read_subject_info(data_file):
 
 def read_block_metadata(data_file):
     """ Reads all metadata from the given nix file.
-    
+
     :param data_file: the path of the nix file.
     :return metadata: a dictionary containing all metadata.
     """
@@ -83,6 +83,68 @@ def read_block_metadata(data_file):
     metadata = nix_metadata_to_dict(m)
     f.close()
     return metadata
+
+
+def read_df(data_file):
+    if not os.path.exists(data_file):
+        print("File not found %s" % data_file)
+        return -9999
+    df = -9999
+    before = -9999
+    after = -9999
+    duration = -9999
+    with open(data_file, 'r') as tf:
+        lines = tf.readlines()
+    for l in lines:
+        if "#" not in l and len(l.strip()) > 0:
+            break
+        if "delta f" in l.lower():
+            df = float(l.split(":")[-1].strip()[:-2])
+        if "before" in l.lower():
+            val = l.split(':')[-1].strip()
+            before = float(val[:val.index('s')])
+        if "after" in l.lower():
+            val = l.split(":")[-1].strip()
+            after = float(val[:val.index('s')])
+        if "duration" in l.lower():
+            val = l.split(":")[-1].strip()
+            duration = float(val[:val.index('s')])
+    return df, before, after, duration
+
+
+def read_jar_data2(data_file, count=-1):
+    if not os.path.exists(data_file):
+        print("File  not found %s!" % data_file)
+        return None, None
+
+    dfs = []
+    data = []
+    path = data_file[:data_file.rindex('/')]
+    f = nix.File.open(data_file, nix.FileMode.ReadOnly)
+    b = f.blocks[0]
+    tag = None
+
+    for i, tag in enumerate(b.multi_tags):
+        if "ManualJAR" in tag.name:
+            number = int(tag.name.split('-')[-1])
+            number = str(number) if number > 9 else "0%i" % number
+            jar_file_name = "%s/manualjar-eod-%s.dat" % (path, number)
+            if os.path.exists(jar_file_name):
+                df, before, after, duration = read_df(jar_file_name)
+            start_time = tag.positions[:][0]
+            duration = tag.extents[:][0]
+            events_array = b.data_arrays['EOD_events']
+            times = events_array[:]
+            eod_times = times[(times > (start_time - before)) & (times < (start_time + duration + after))] - start_time
+
+            data.append(eod_times)
+            dfs.append(df)
+
+            if count > -1 and i >= (count-1):
+                break
+        tag = None
+    f.close()
+    return data, dfs
 
 
 def read_jar_data(data_file, pre_stimulus_duration=5., post_stimulus_duration=20):
@@ -141,9 +203,10 @@ def read_jar_data(data_file, pre_stimulus_duration=5., post_stimulus_duration=20
 
 if __name__ == "__main__":
     # make sure to close the file before exiting! Take care, when using embed!
-    datafile = '../configs/wave/2016-11-11-aa-wombat/2016-11-11-aa-wombat.nix.h5'
+    datafile = '/Users/jan/zwischenlager/efish_kurs/data/chirps/2017-11-27-aa-amnesix/2017-11-27-aa-amnesix.nix'
     # t,e = read_eod(datafile)
     # events = read_eod_events(datafile)
-    info = read_subject_info(datafile)
-    b_info = read_block_metadata(datafile)
+    # info = read_subject_info(datafile)
+    # b_info = read_block_metadata(datafile)
+    data, dfs = read_jar_data2(datafile)
     embed()
